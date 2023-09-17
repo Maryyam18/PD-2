ee06866c1fe9c90a4e7d19d40ba95997915b8d7f
Mayyam18 <maryyamfakhar39@gmail.com>
2023-09-17 05:44:02 -0700

    second assigment

---
+++ task13-CP.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main(){ system("color 75");
+	cout << "                    +--^------------,---------,-------,----------^-,    "<<endl;
+	cout << "                     | |||||||||||   '---------'       |            O   "<<endl;
+	cout << "                     '+--------------------------------^------------|   "<<endl;
+	cout << "                       ',-----------,----------,------------------'     "<<endl;
+	cout << "                         / XXXXXXXX /'|        /'                       "<<endl;
+	cout << "                        / XXXXXXXX /  |       /'                        "<<endl;
+	cout << "                       / XXXXXXXX /'--------'                           "<<endl;
+	cout << "                      / XXXXXXXX /                                      "<<endl;
+	cout << "                     / XXXXXXXX /                                       "<<endl;
+	cout << "                    (----------(                                        "<<endl;
+	cout << "                     '--------'                                         "<<endl;
+} 
